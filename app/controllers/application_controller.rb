a270868eb033c5eb4e4c7fbf51e797efbc02c5f0
Bon Saurus <bon.saurus.bon@gmail.com>
2022-12-03 00:36:25 +0900

    Create hello method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  def hello
+    render html: '¡Hola, mundo!'
+  end
+
+  def goodbye
+    render html: 'goodbye, world!'
+  end
 end
